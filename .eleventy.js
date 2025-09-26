582229821aaa356322e013898f6af44d2e834971
p10pro <15219491+jkaps9@users.noreply.github.com>
2025-09-26 17:29:15 -0400

    add rss feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const rssPlugin = require('@11ty/eleventy-plugin-rss');
+
 // Filters
 const dateFilter = require("./src/filters/date-filter.js");
 const w3DateFilter = require("./src/filters/w3-date-filter.js");
@@ -11,6 +13,9 @@ module.exports = (config) => {
 
   // Set directories to pass through to the dist folder
   config.addPassthroughCopy("./src/images/");
+
+// Plugins
+config.addPlugin(rssPlugin);
 
   // Returns work items, sorted by display order
   config.addCollection("work", (collection) => {
@@ -29,12 +34,12 @@ module.exports = (config) => {
     return [...collection.getFilteredByGlob("./src/posts/*.md")].reverse();
   });
 
-	// Returns a list of people ordered by filename
-config.addCollection('people', (collection) => {
-	return collection.getFilteredByGlob('./src/people/*.md').sort((a, b) => {
-		return Number(a.fileSlug) > Number(b.fileSlug) ? 1 : -1;
-	});
-});
+  // Returns a list of people ordered by filename
+  config.addCollection("people", (collection) => {
+    return collection.getFilteredByGlob("./src/people/*.md").sort((a, b) => {
+      return Number(a.fileSlug) > Number(b.fileSlug) ? 1 : -1;
+    });
+  });
 
   return {
     markdownTemplateEngine: "njk",

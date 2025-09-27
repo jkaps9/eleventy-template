135dbc3d041e21f3208ffc2477b8120b6e101087
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-26 23:21:57 -0400

    Revert "update script to run gulp and eleventy"
    This reverts commit 69cb60e26764ed11b1e42d5329a86916c4819bed.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
-const { parallel, watch } = require("gulp");
+const { parallel, watch } = require('gulp');
 
 // Pull in each task
-const sass = require("./gulp-tasks/sass.js");
+const sass = require('./gulp-tasks/sass.js');
 
 // Set each directory and contents that we want to watch and
 // assign the relevant task. `ignoreInitial` set to true will
 // prevent the task being run when we run `gulp watch`, but it
 // will run when a file changes.
 const watcher = () => {
-  watch("./src/scss/**/*.scss", { ignoreInitial: true }, sass);
+	watch('./src/scss/**/*.scss', { ignoreInitial: true }, sass);
 };
 
 // The default (if someone just runs `gulp`) is to run each task in parallel

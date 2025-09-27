7a5dc979349f01ddbc17b06b22e191ccbe7850fd
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-27 19:41:10 -0400

    Revert "add fonts and readme"
    This reverts commit ba9bb22c1fe0a8cad2559137f0f3733162c259f5.

--- .eleventy.js
+++ .eleventy.js
@@ -13,7 +13,6 @@ module.exports = (config) => {
 
   // Set directories to pass through to the dist folder
   config.addPassthroughCopy("./src/images/");
-  config.addPassthroughCopy("./src/fonts/");
 
   // Plugins
   config.addPlugin(rssPlugin);

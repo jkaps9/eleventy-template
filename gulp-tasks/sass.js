f55759bf620e6ea1f59ca536089191812b302558
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-27 19:41:13 -0400

    Revert "update gulp require"
    This reverts commit 47a8866f82525b332ee56a85f81750e14568dc3a.

--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -1,7 +1,9 @@
 const { dest, src } = require("gulp");
 const cleanCSS = require("gulp-clean-css");
-const sassProcessor = require("gulp-sass")(require("sass"));
+const sassProcessor = require("gulp-sass");
 
+// We want to be using canonical Sass, rather than node-sass
+sassProcessor.compiler = require("sass");
 // Flags whether we compress the output etc
 const isProduction = process.env.NODE_ENV === "production";
 
@@ -31,7 +33,6 @@ const calculateOutput = ({ history }) => {
 
   return response;
 };
-
 // The main Sass method grabs all root Sass files,
 // processes them, then sends them to the output calculator
 const sass = () => {

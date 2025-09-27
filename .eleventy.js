ba9bb22c1fe0a8cad2559137f0f3733162c259f5
Josh <15219491+jkaps9@users.noreply.github.com>
2025-09-27 00:10:39 -0400

    add fonts and readme

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,7 @@ module.exports = (config) => {
 
   // Set directories to pass through to the dist folder
   config.addPassthroughCopy("./src/images/");
+  config.addPassthroughCopy("./src/fonts/");
 
   // Plugins
   config.addPlugin(rssPlugin);
